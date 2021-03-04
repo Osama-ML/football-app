b3e543d375f942013d0a57be6e1df5875763f719
blueC4P <osamag4g@gmail.com>
2021-03-04 23:02:51 +0000

    Improve Player view

--- src/views/Player.js
+++ src/views/Player.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import api from "../api";
 
-export const Player = props => {
+export const Player = (props) => {
+  const player = (props.location && props.location.state) || {};
 
-    const player = (props.location && props.location.state) || {};
+  const [team, setAllTeam] = useState({});
 
-    return (
-        <div>
-            <img
-                src={player["Avatar"]}
-                width="250"
-                height="250"
-                alt="imagen de la liga"
-              ></img>
-            <h2>Player {player["Nombre del Jugador"]}</h2>
-        </div>
-    )
-}
+  const teamArr = async () => {
+    await api
+      .get("/teams")
+      .then((response) =>
+        setAllTeam(
+          response.data.filter((team) => team["id"] === player["teamId"])
+        )
+      );
+  };
+
+  useEffect(() => {
+    teamArr();
+  }, [setAllTeam]);
+
+  return (
+    <div>
+      <h2>Player {player["Nombre del Jugador"]}</h2>
+      <img
+        src={player["Avatar"]}
+        width="250"
+        height="250"
+        alt="imagen de la liga"
+      ></img>
+
+      <h3>Team: {team[0]["Nombre del equipo"]}</h3>
+      <img
+        src={team[0]["Logo del Equipo"]}
+        width="150"
+        height="150"
+        alt="imagen del equipo"
+      ></img>
+    </div>
+  );
+};
